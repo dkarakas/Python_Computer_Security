7f1f2e27dd81884958874209e997aab8720d1c64
Dimcho Karakashev <dimchokarakashev@gmail.com>
2017-01-17 21:45:25 -0500

    Finished but need to confirm I did the assignment right

--- hw01/cipher.py
+++ hw01/cipher.py
@@ -5,10 +5,10 @@ import sys
 #reading key file
 KEY_FILE = open("key.txt",'r')          #opening a file containing the key
 PLAIN_TEXT = open("input.txt",'r')      #opening a file containing the plain text
-OUTPUT_TEXT = open("write.txt",'w')     #opening a file to save the encryption
+OUTPUT_TEXT = open("output.txt",'w')     #opening a file to save the encryption
 
 key = KEY_FILE.read()                   #reading the whole key value
-key_len = len(key) - 1                  #gets the length of the key & - 1 to get rid of the new line
+key_len = len(key) - 2                  #gets the length of the key & - 1 to get rid of the new line
 curIndexKey = 0                         #initialize the key index to encrypt
 
 
@@ -19,17 +19,18 @@ while True:
     if( curIndexKey >= key_len):
         curIndexKey = 0
     pos_to_move = abs(ord('A') - ord(key[curIndexKey]))     #computing how much should i move the encryption key
-    curIndexKey += 1                    #moving through the key
     encryptCh = ord(chIn) + pos_to_move
     if( encryptCh > 122):
-        encryptCh = ( encrypthCh - 122 ) + 65
-    if ( encrypthCh > 90 ) and ( encrypthCh < 97 ):
+        encryptCh = ( encryptCh - 122 ) + 65
+    if ( encryptCh > 90 ) and ( encryptCh < 97 ):
         encryptCh = encryptCh + 6
-    OUTPUT_TEXT.write(char(encryptCh)
+    OUTPUT_TEXT.write(chr(encryptCh))
+    print "input: " + chIn  + " "\
+            "key: " + key[curIndexKey] + " " + \
+            str(pos_to_move) + "Encrypted char: " + chr(encryptCh)
+    curIndexKey += 1                    #moving through the key
 
-    #sys.stdout.write(str(pos_to_move) + ' '+ chIn + ' ' + key[curIndexKey] + ' ')
-    #print curIndexKey
-
-KEY_TEXT.close()
+#Closing files
+KEY_FILE.close()
 PLAIN_TEXT.close()
 OUTPUT_TEXT.close()
