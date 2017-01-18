0e799cad8f253902949393f42f411df26e515f83
Dimcho Karakashev <dimchokarakashev@gmail.com>
2017-01-17 21:29:36 -0500

    Debugging left

--- hw01/cipher.py
+++ hw01/cipher.py
@@ -5,24 +5,31 @@ import sys
 #reading key file
 KEY_FILE = open("key.txt",'r')          #opening a file containing the key
 PLAIN_TEXT = open("input.txt",'r')      #opening a file containing the plain text
+OUTPUT_TEXT = open("write.txt",'w')     #opening a file to save the encryption
 
 key = KEY_FILE.read()                   #reading the whole key value
-key_len = len(key)                      #gets the length of the key
+key_len = len(key) - 1                  #gets the length of the key & - 1 to get rid of the new line
 curIndexKey = 0                         #initialize the key index to encrypt
+
 
 while True:
     chIn = PLAIN_TEXT.read(1)           #reading one char at a time
-
-    if not chIn:                        #checks if we had reached the end of the file
+    if( not chIn ) or ( chIn == '\n' ):    #checks if we had reached the end of the file
         break
-
-    if(curIndexKey >= curIndexKey):
+    if( curIndexKey >= key_len):
         curIndexKey = 0
+    pos_to_move = abs(ord('A') - ord(key[curIndexKey]))     #computing how much should i move the encryption key
+    curIndexKey += 1                    #moving through the key
+    encryptCh = ord(chIn) + pos_to_move
+    if( encryptCh > 122):
+        encryptCh = ( encrypthCh - 122 ) + 65
+    if ( encrypthCh > 90 ) and ( encrypthCh < 97 ):
+        encryptCh = encryptCh + 6
+    OUTPUT_TEXT.write(char(encryptCh)
 
+    #sys.stdout.write(str(pos_to_move) + ' '+ chIn + ' ' + key[curIndexKey] + ' ')
+    #print curIndexKey
 
-
-"""
-for inputa in read:
-    print(inputa+ "\n" )
-"""
-
+KEY_TEXT.close()
+PLAIN_TEXT.close()
+OUTPUT_TEXT.close()
