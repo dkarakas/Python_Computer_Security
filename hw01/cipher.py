7ba8e25e384010b8d03fa42e776bf00ea4ee5358
dimcho <dimcho@localhost.localdomain>
2017-01-17 20:42:37 -0500

    successfully reading char at time from a file

--- hw01/cipher.py
+++ hw01/cipher.py
@@ -1,14 +1,23 @@
 #! /usr/bin/env python
 
+import sys
+
 #reading key file
 KEY_FILE = open("key.txt",'r')          #opening a file containing the key
 PLAIN_TEXT = open("input.txt",'r')      #opening a file containing the plain text
 
-key = KEY_FILE.read();                  #reading the whole key value
+key = KEY_FILE.read()                   #reading the whole key value
+key_len = len(key)                      #gets the length of the key
+curIndexKey = 0                         #initialize the key index to encrypt
 
 while True:
-    chIn = PLAIN_TEXT.read(1);          #reading one char at a time
+    chIn = PLAIN_TEXT.read(1)           #reading one char at a time
 
+    if not chIn:                        #checks if we had reached the end of the file
+        break
+
+    if(curIndexKey >= curIndexKey):
+        curIndexKey = 0
 """
 for inputa in read:
     print(inputa+ "\n" )
