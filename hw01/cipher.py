6663c24e79d2331c1095a29cbe70639b3eb8b7db
Dimcho Karakashev <dimchokarakashev@gmail.com>
2017-01-21 18:01:50 -0500

    Added initial file for hw02

--- hw01/cipher.py
+++ hw01/cipher.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python
 
+#Dimcho Karakashev
+#01/18/17
+#ece404 hw01
+#assumption that the only input includes A-Z and a-z. Same applys for the key
+
 import sys
 
 #reading key file
@@ -14,26 +19,30 @@ curIndexKey = 0                         #initialize the key index to encrypt
 
 while True:
     chIn = PLAIN_TEXT.read(1)           #reading one char at a time
-    if( not chIn ) or ( chIn == '\n' ):    #checks if we had reached the end of the file
+    if( not chIn ) or ( chIn == '\n' ): #checks if we had reached the end of the file
         break
-    if( curIndexKey > key_len):
+    if( curIndexKey > key_len):         #interating through the key in the file
         curIndexKey = 0 
     
     #computing how much should i move the encryption key
-    if( (ord(key[curIndexKey]) > 64) and (ord(key[curIndexKey]) < 91) ): #if it is uppercase
+    if( (ord(key[curIndexKey]) > 64) and (ord(key[curIndexKey]) < 91) ):    #if it is uppercase
         pos_to_move = abs(ord('A') - ord(key[curIndexKey]))
     elif( (ord(key[curIndexKey]) > 96) and (ord(key[curIndexKey]) < 173) ): #if it is lowercase
         pos_to_move = abs(ord('A') - ord(key[curIndexKey]) + 6)
 
     encryptCh = ord(chIn) + pos_to_move
-    if( encryptCh > 122):
-        encryptCh = ( encryptCh - 122 ) + 65
-    #if ( encryptCh > 90 ) and ( encryptCh < 97 ):
-    #    encryptCh = encryptCh + 6
+
+    if( encryptCh > 122 ):                              #dealing with the values higher than the highest lower case char
+        encryptCh = encryptCh - 58 
+    if( ( encryptCh > 90 ) and ( encryptCh < 97 ) ):    #taking care of the values between upper and lower case
+        encryptCh = encryptCh + 6
+
     OUTPUT_TEXT.write(chr(encryptCh))
-    print("input: " + chIn  + " "\
-            "key: " + key[curIndexKey] + " " + \
-            str(pos_to_move) + "Encrypted char: " + chr(encryptCh))
+
+    #debugging statement
+    #print("input: " + chIn  + " "\
+    #        "key: " + key[curIndexKey] + " " + \
+    #        str(pos_to_move) + "Encrypted char: " + chr(encryptCh) + " " + str(encryptCh))
     curIndexKey += 1                    #moving through the key
 
 #Closing files
